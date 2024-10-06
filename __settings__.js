25488d089304052e228464cf246731616c23d38f
Mariru1z <76128232+Mariru1z@users.noreply.github.com>
2024-10-05 18:43:17 -0600

    commit2

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2085478.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 198541478, 198541479, 198541502, 198541482, 198541483, 198541484, 198541485, 198541486, 198541488, 198541489, 198541490, 198541491, 198541493, 198541494 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/198541499/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/198541500/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/198541498/1/ammo.js', 'preload' : true},
+];
